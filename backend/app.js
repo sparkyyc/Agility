43481fe253ed54adb41ba98a1c3a5e20b133e269
Christa Sparks <christa@Christas-MacBook-Pro.local>
2019-01-20 15:49:18 -0700

    hook up postgraphile with express

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const { postgraphile } = require('postgraphile')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -13,6 +14,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(postgraphile(process.env.DATABASE_URL || "postgres:///agility-dev", 'public', {
+    graphiql: true
+}));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
